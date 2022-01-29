8e8bf2a6db6f2c1e0fc45f61e47f3ee506f1056b
speakman <pierredumas90000@gmail.com>
2022-01-29 20:19:26 +0100

    MAJ nettoyage + ajout random

--- js/script.js
+++ js/script.js
@@ -2,82 +2,78 @@ $(function(){
 
 
 $("#xin").click(function(){
-    var x = $("#xin");
+    var x = "#xin";
     equipe(x, xin);
-    showButton(); 
 });
 
 $("#jinx").click(function(){
-    var j = $("#jinx");
+    var j = "#jinx";
     equipe(j, jinx);
-    showButton();
 });
 
 $("#wukong").click(function(){
-    var w = $("#wukong");
+    var w = "#wukong";
     equipe(w, wukong);
-    showButton();
 });
 
 $("#yi").click(function(){
-    var yi = $("#yi");
+    var yi = "#yi";
     equipe(yi, yii);
-    showButton();
 });
 
 $("#garen").click(function(){
-    var g = $("#garen");
+    var g = "#garen";
     equipe(g, garen);
-    showButton();
 });
 
 $("#fizz").click(function(){
-    var f = $("#fizz");
+    var f = "#fizz";
     equipe(f, fizz);
-    showButton();
 });
 
 $("#ezreal").click(function(){
-    var e = $("#ezreal");
+    var e = "#ezreal";
     equipe(e, ezreal);
-    showButton();
 });
 
 $("#tresh").click(function(){
-    var t = $("#tresh");
+    var t = "#tresh";
     equipe(t, tresh);
-    showButton();
 });
 
 $("#ivern").click(function(){
-    var i = $("#ivern");
+    var i = "#ivern";
     equipe(i, ivern);
-    showButton();
 });
 
 $("#yasuo").click(function(){
-    var y = $("#yasuo");
+    var y = "#yasuo";
     equipe(y, yasuo);
-    showButton();
+    
 });
 
+
+let temps = 30;
 
 $("#go").click(function(){
 
     $("#go").prop('disabled', true);
     const interval = setInterval(timer, 1000)
-    var i = 30;
 
-        function timer() {
-            $("#timer").html(i)
-            i--
-                if (i == -1) {
-                    clearInterval(interval)
-                    $("#timer").css("visibility", "hidden")
-                    spawnitem()
-                    runbagare();
-                }
+    function timer() {
+        $("#timer").html(temps)
+        temps--;
+        nivExp()
+        spawnitem()
+            if (temps == -1) {
+                clearInterval(interval)
+                $("#timer").css("visibility", "hidden")
+                runbagare();
+                
             }
+        }
+    
+    
 })
 
 $('#resett').click(function(){
@@ -86,15 +82,6 @@ $('#resett').click(function(){
 
 
 /////////////////////////////////////////////////////////////////
-function showButton () {
-var r = $(".rouge").length;
-var b = $(".blue").length;
-
-if (r == 5 && b == 5) {
-        $("#go").removeClass("run")
-        $('#title-gen').text("Bienvenue sur league of legend du pauvre")
-    }
-}
 
 function equipe(a,o) {
     var m = $(".blue").length;
@@ -102,21 +89,28 @@ function equipe(a,o) {
     var a;
     var o;
 
-    $(a).removeClass("champs")
+    $(a).removeClass('champs')
     $(a).addClass('blue')
 
-    if (m <= 4) {
+    if (m < 5) {
         $('#ar_blue').append($(a))
-        bleu.push(o.attaque);
+        bleu.push(o);
     } else if (q <= 4) {
         $('#ar_red').append($(a))
         $(a).removeClass('blue')
         $(a).addClass("rouge")
-        rouge.push(o.attaque);
+        rouge.push(o);
     }
+    
+    if (m == 5 && q == 4) {
+        $("#go").removeClass("run")
+        $('#title-gen').text("Bienvenue sur league of legend du pauvre")
+    }
+
 }
 
 function runbagare() {
+
     const reducer = (previousValue, currentValue) => previousValue + currentValue;
     if (bleu.reduce(reducer) < rouge.reduce(reducer)){
             $('#victory').text("ROUGE GAGNE").css("visibility", "visible").addClass("roug");
@@ -126,70 +120,81 @@ function runbagare() {
 }
 
 function spawnitem() {
-    if(rand > 0 && rand < 4999) {
-        bleu.push(item.botte);
-        rouge.push(item.botte);
+    
+    if((randItem() > 0 && randItem() < 4999) && temps == randItem_b()) {
+        bleu.push(botte);
         $('#gauche').append("<div class='botte'></div>")
+    }else if ((randItem() > 0 && randItem() < 4999) && temps == randItem_r()) {
+        rouge.push(botte);
         $('#droite').append("<div class='botte'></div>")
     }
     
-    if(rand > 500 && rand < 1000) {
-        bleu.push(item.cleaver);
+    if((randItem() > 0 && randItem() < 1000) && temps == randItem_b()) {
+        bleu.push(cleaver);
         $('#gauche').append("<div class='cleaver'></div>")
-    } 
-    
-    if(rand > 0 && rand < 500) {
-        rouge.push(item.cleaver);
+    } else if((randItem() > 0 && randItem() < 1000) && temps == randItem_r()) {
+        rouge.push(cleaver);
         $('#droite').append("<div class='cleaver'></div>")
     }
 }
 
 
+function nivExp() {
+
+    if (temps == randExp_b()) {
+        $('#gauche').append("<h3 style='color:white'> ",randChamp()," prend 1 niveaux </h3>")
+    }
+
+    if (temps == randExp_r()) {
+        $('#droite').append("<h3 style='color:white'> ",randChamp()," prend 1 niveaux </h3>")
+        }
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
+const champions = [xin=9.63, jinx=11.01, wukong=9.78, yii=10.41, garen=9.88, fizz=10.63, ezreal=12.51, tresh=3.61, ivern=4.6, yasuo=9.78];
 
-const xin = {
-    attaque: 9.63
-}
-
-const jinx = {
-    attaque: 11.01
-}
-const wukong = {
-    attaque: 9.78
-}
-const yii = {
-    attaque: 10.41
-}
-const garen = {
-    attaque: 9.88
-}
-const fizz = {
-    attaque: 10.63
-}
-const ezreal = {
-    attaque: 12.51
-}
-const tresh = {
-    attaque: 3.61
-}
-const ivern = {
-    attaque: 4.6
-}
-const yasuo = {
-    attaque: 9.78
-}
-
-var rouge = [];
-var bleu = [];
+const rouge = [];
+const bleu = [];
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-var rand = Math.floor((Math.random() * 5000) + 1);
+function randItem() {
+    const itemRand = Math.floor((Math.random() * 5000) + 1);
+    return itemRand;
+};
 
-
-const item = {
-    botte: 2,
-    cleaver: 5
+function randNiv() {
+    const randNiv = Math.floor((Math.random() * 300) + 1);
+    return randNiv;
 }
+
+function randChamp() {
+    const champRand = champions[Math.floor(Math.random() * champions.length)];
+    return champRand;
+}
+
+function randItem_r() {
+    const randTime_item_r = Math.floor((Math.random() * 29) + 1);
+    return randTime_item_r;
+}
+
+function randItem_b() {
+    const randTime_item_b = Math.floor((Math.random() * 29) + 1);
+    return randTime_item_b;
+}
+
+function randExp_r() {
+    const randTime_exp_r = Math.floor((Math.random() * 29) + 1);
+    return randTime_exp_r;
+}
+
+function randExp_b() {
+    const randTime_exp_b = Math.floor((Math.random() * 29) + 1);
+    return randTime_exp_b;
+}
+
+
+
+const items = [botte=2, cleaver=5]
 
 })

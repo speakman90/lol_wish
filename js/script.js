b9de9ead577f424ca2fd2fd49e71a47ef74f270d
speakman <pierredumas90000@gmail.com>
2022-01-31 10:16:46 +0100

    Patch bug + Add exp

--- js/script.js
+++ js/script.js
@@ -16,9 +16,9 @@ $("#wukong").click(function(){
     equipe(w, wukong);
 });
 
-$("#yi").click(function(){
-    var yi = "#yi";
-    equipe(yi, yii);
+$("#master").click(function(){
+    var m = "#master";
+    equipe(m, master);
 });
 
 $("#garen").click(function(){
@@ -53,7 +53,7 @@ $("#yasuo").click(function(){
 });
 
 
-let temps = 30;
+let temps = 5;
 
 $("#go").click(function(){
 
@@ -69,7 +69,6 @@ $("#go").click(function(){
                 clearInterval(interval)
                 $("#timer").css("visibility", "hidden")
                 runbagare();
-                
             }
         }
     
@@ -94,12 +93,15 @@ function equipe(a,o) {
 
     if (m < 5) {
         $('#ar_blue').append($(a))
-        bleu.push(o);
+        bleu.push(o.attaque);
+        bleup.push(o.pseudo);
+        console.log(bleu)
     } else if (q <= 4) {
         $('#ar_red').append($(a))
         $(a).removeClass('blue')
         $(a).addClass("rouge")
-        rouge.push(o);
+        rouge.push(o.attaque);
+        rougep.push(o.pseudo)
     }
     
     if (m == 5 && q == 4) {
@@ -128,11 +130,10 @@ function spawnitem() {
         rouge.push(botte);
         $('#droite').append("<div class='botte'></div>")
     }
-    
-    if((randItem() > 0 && randItem() < 1000) && temps == randItem_b()) {
+    if((randItem() > 0 && randItem() < 4999) && temps == randItem_b()) {
         bleu.push(cleaver);
         $('#gauche').append("<div class='cleaver'></div>")
-    } else if((randItem() > 0 && randItem() < 1000) && temps == randItem_r()) {
+    } else if((randItem() > 0 && randItem() < 4999) && temps == randItem_r()) {
         rouge.push(cleaver);
         $('#droite').append("<div class='cleaver'></div>")
     }
@@ -142,20 +143,62 @@ function spawnitem() {
 function nivExp() {
 
     if (temps == randExp_b()) {
-        $('#gauche').append("<h3 style='color:white'> ",randChamp()," prend 1 niveaux </h3>")
+        $('#gauche').append("<h3> ",randChamp_b()," prend 1 niveaux </h3>").css("color","white")
     }
-
     if (temps == randExp_r()) {
-        $('#droite').append("<h3 style='color:white'> ",randChamp()," prend 1 niveaux </h3>")
-        }
+        $('#droite').append("<h3 style='color:white'> ",randChamp_r()," prend 1 niveaux </h3>").css("color","white")
+    }
 }
 
 
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-const champions = [xin=9.63, jinx=11.01, wukong=9.78, yii=10.41, garen=9.88, fizz=10.63, ezreal=12.51, tresh=3.61, ivern=4.6, yasuo=9.78];
+
+let xin = new Object()
+    xin.pseudo= "xin";
+    xin.attaque= 9.63;
+
+let jinx = new Object();
+    jinx.pseudo= "jinx";
+    jinx.attaque= 11.01;
+
+let wukong = new Object();
+    wukong.pseudo= "wukong";
+    wukong.attaque= 9.78;
+
+let master = new Object();
+    master.pseudo= "maître yi";
+    master.attaque= 10.41;
+
+let garen = new Object();
+    garen.pseudo= "garen";
+    garen.attaque= 9.88;
+
+let fizz = new Object();
+    fizz.pseudo= "fizz";
+    fizz.attaque= 10.63;
+
+let ezreal = new Object();
+    ezreal.pseudo= "ezreal";
+    ezreal.attaque= 12.51;
+
+let tresh = new Object();
+    tresh.pseudo= "tresh";
+    tresh.attaque= 3.61;
+
+let ivern = new Object();
+    ivern.pseudo= "ivern";
+    ivern.attaque= 4.6;
+
+let yasuo = new Object();
+    yasuo.pseudo= "yasuo";
+    yasuo.attaque= 9.78;
 
 const rouge = [];
+const rougep = [];
 const bleu = [];
+const bleup = [];
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 function randItem() {
@@ -163,33 +206,33 @@ function randItem() {
     return itemRand;
 };
 
-function randNiv() {
-    const randNiv = Math.floor((Math.random() * 300) + 1);
-    return randNiv;
+function randChamp_b() {
+    const champRand_b = bleup[Math.floor(Math.random() * bleup.length)];
+    return champRand_b;
 }
 
-function randChamp() {
-    const champRand = champions[Math.floor(Math.random() * champions.length)];
-    return champRand;
+function randChamp_r() {
+    const champRand_r = rougep[Math.floor(Math.random() * rougep.length)];
+    return champRand_r;
 }
 
 function randItem_r() {
-    const randTime_item_r = Math.floor((Math.random() * 29) + 1);
+    const randTime_item_r = Math.floor((Math.random() * 4) + 1);
     return randTime_item_r;
 }
 
 function randItem_b() {
-    const randTime_item_b = Math.floor((Math.random() * 29) + 1);
+    const randTime_item_b = Math.floor((Math.random() * 4) + 1);
     return randTime_item_b;
 }
 
 function randExp_r() {
-    const randTime_exp_r = Math.floor((Math.random() * 29) + 1);
+    const randTime_exp_r = Math.floor((Math.random() * 4) + 1);
     return randTime_exp_r;
 }
 
 function randExp_b() {
-    const randTime_exp_b = Math.floor((Math.random() * 29) + 1);
+    const randTime_exp_b = Math.floor((Math.random() * 4) + 1);
     return randTime_exp_b;
 }
 
